12b1ac32b3bb4594cb80ae68deff3cb8102d480f
xxzj990 <xxzj990@qq.com>
2020-12-30 10:11:41 +0800

    添加说明

--- [0002]array-sort-mixed.dart
+++ [0002]array-sort-mixed.dart
@@ -1,5 +1,12 @@
 import 'dart:math' as Math;
 
+///
+/// [0001]进阶:
+/// 
+/// 如果给定的数组已经排好序呢？将如何优化你的算法呢？
+/// 思路：设定两个为0的指针，比较两个指针的元素是否相等。如果指针的元素相等，我们将两个指针一起向后移动，并且将相等的元素放入空白数组。
+/// 
+
 void main() {
   var a = [5, 7, 8, 3, 3];
   var b = [7, 4, 3, 8, 9, 8, 3];
@@ -8,6 +15,11 @@ void main() {
   b.sort();
   print('a:$a ,b:$b');
 
+  var ret = mix(a, b);
+  print('ret:$ret');
+}
+
+List mix(List a, List b) {
   var lenMax = Math.max(a.length, b.length);
   var lenMin = Math.min(a.length, b.length);
   var ret = [];
@@ -26,5 +38,5 @@ void main() {
       n++;
     }
   }
-  print('ret:$ret');
+  return ret;
 }
